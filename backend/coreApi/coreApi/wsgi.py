5aa0db9925216528691564ca95b67944b40a076b
Rochdi Khalid <87553163+rochdikhalid@users.noreply.github.com>
2021-11-26 09:42:44 +0100

    Update wsgi.py

--- backend/coreApi/coreApi/wsgi.py
+++ backend/coreApi/coreApi/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for koprApi project.
+WSGI config for coreApi project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'koprApi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coreApi.settings')
 
 application = get_wsgi_application()
