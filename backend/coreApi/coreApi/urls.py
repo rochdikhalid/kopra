f22a89283f29ed20c8b8e339e303c65151ca26f9
Rochdi Khalid <87553163+rochdikhalid@users.noreply.github.com>
2021-11-27 14:54:17 +0100

    Update urls.py

--- backend/coreApi/coreApi/urls.py
+++ backend/coreApi/coreApi/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/stations/', include('stations.urls')),
+    path('api/drivers/', include('drivers.urls')),
 ]
