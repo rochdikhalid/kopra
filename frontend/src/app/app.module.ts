2eb49ccd0f1258d173ef65cc2c30652041b7e02e
Rochdi Khalid <87553163+rochdikhalid@users.noreply.github.com>
2021-11-30 13:05:51 +0100

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+/* Service */
+import { SharedService } from './shared.service';
+import { AppRoutingModule, RoutingComponents } from './app-routing.module';
+/* Parent component */
 import { AppComponent } from './app.component';
+/* Panel */
+import { PanelComponent } from './panel/panel.component';
+import { HeaderComponent } from './panel/header/header.component';
+import { SidebarComponent } from './panel/sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoutingComponents,
+    PanelComponent,
+    HeaderComponent,
+    SidebarComponent,
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [SharedService],
+  // The application is going to start running from this component
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
