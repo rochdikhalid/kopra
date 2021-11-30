e832c1918c93ea4458e0393414b1572de532e226
Rochdi Khalid <87553163+rochdikhalid@users.noreply.github.com>
2021-11-30 13:07:10 +0100

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+/* Home */
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './home/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+    ] 
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+
+export const RoutingComponents = [
+  HomeComponent,
+  DashboardComponent,
+]
+
