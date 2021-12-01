fae5ea3d1c12f7777d3c5a9deadddf0602003b78
Rochdi Khalid <87553163+rochdikhalid@users.noreply.github.com>
2021-12-01 13:09:16 +0100

    Update shared.service.ts

--- frontend/src/app/shared.service.ts
+++ frontend/src/app/shared.service.ts
@@ -18,115 +18,115 @@ export class SharedService {
 
   // GET ALL STATIONS NAMES
   getStationsList(): Observable<any> {
-    return this.http.get(`${this.APIUrl}/stations`)
+    return this.http.get(`${this.APIUrl}/api/stations`)
   };
 
   // ADD STATION
   addStation(station: Object): Observable<object> {
-    return this.http.post(`${this.APIUrl}/stations/`, station)
+    return this.http.post(`${this.APIUrl}/api/stations/`, station)
   };
 
   // UPDATE STATION
   updateStation(value: any): Observable<object>{
-    return this.http.put(`${this.APIUrl}/stations/`, value);
+    return this.http.put(`${this.APIUrl}/api/stations/`, value);
   }
 
   // DELETE STATION
   deleteStation(id: number): Observable<any> {
-    return this.http.delete(`${this.APIUrl}/stations/${id}`)
+    return this.http.delete(`${this.APIUrl}/api/stations/${id}`)
   };
 
   // GET STATIONS' NAMES
   getStationsNames(): Observable<any> {
-    return this.http.get(`${this.APIUrl}/stations`);
+    return this.http.get(`${this.APIUrl}/api/stations`);
   }
 
   /* DRIVERS */
 
   // GET ALL DRIVERS NAMES
   getDriversList(): Observable<any> {
-    return this.http.get(`${this.APIUrl}/drivers`)
+    return this.http.get(`${this.APIUrl}/api/drivers`)
   };
 
   // ADD DRIVER
   addDriver(driver: Object): Observable<object> {
-    return this.http.post(`${this.APIUrl}/drivers/`, driver)
+    return this.http.post(`${this.APIUrl}/api/drivers/`, driver)
   };
 
   // UPDATE DRIVER
   updateDriver(value: any): Observable<object>{
-    return this.http.put(`${this.APIUrl}/drivers/`, value);
+    return this.http.put(`${this.APIUrl}/api/drivers/`, value);
   }
 
   // DELETE DRIVER
   deleteDriver(id: number): Observable<any> {
-    return this.http.delete(`${this.APIUrl}/drivers/${id}`)
+    return this.http.delete(`${this.APIUrl}/api/drivers/${id}`)
   };
 
   /* FLEETS */
 
   // GET ALL FLEETS NAMES
   getFleetsList(): Observable<any> {
-    return this.http.get(`${this.APIUrl}/fleets`)
+    return this.http.get(`${this.APIUrl}/api/fleets`)
   };
 
   // ADD FLEET
   addFleet(fleet: Object): Observable<object> {
-    return this.http.post(`${this.APIUrl}/fleets/`, fleet)
+    return this.http.post(`${this.APIUrl}/api/fleets/`, fleet)
   };
 
   // UPDATE FLEET
   updateFleet(value: any): Observable<object>{
-    return this.http.put(`${this.APIUrl}/fleets/`, value);
+    return this.http.put(`${this.APIUrl}/api/fleets/`, value);
   }
 
   // DELETE FLEET
   deleteFleet(id: number): Observable<any> {
-    return this.http.delete(`${this.APIUrl}/fleets/${id}`)
+    return this.http.delete(`${this.APIUrl}/api/fleets/${id}`)
   };
   
   /* PASSENGERS */
 
   // GET ALL COMMUTERS 
   getCommutersList(): Observable<any> {
-    return this.http.get(`${this.APIUrl}/passengers`)
+    return this.http.get(`${this.APIUrl}/api/passengers`)
   };
 
   // ADD COMMUTER
   addCommuter(commuter: Object): Observable<object> {
-    return this.http.post(`${this.APIUrl}/passengers/`, commuter)
+    return this.http.post(`${this.APIUrl}/api/passengers/`, commuter)
   };
 
   // UPDATE COMMUTER
   updateCommuter(value: any): Observable<object>{
-    return this.http.put(`${this.APIUrl}/passengers/`, value);
+    return this.http.put(`${this.APIUrl}/api/passengers/`, value);
   }
 
   // DELETE COMMUTER
   deleteCommuter(id: number): Observable<any> {
-    return this.http.delete(`${this.APIUrl}/passengers/${id}`)
+    return this.http.delete(`${this.APIUrl}/api/passengers/${id}`)
   };
 
   /* TRIPS */
 
   // GET ALL TRIP NAMES
   getTripsList(): Observable<any> {
-    return this.http.get(`${this.APIUrl}/trips`)
+    return this.http.get(`${this.APIUrl}/api/trips`)
   };
 
   // ADD TRIP
   addTrip(trip: Object): Observable<object> {
-    return this.http.post(`${this.APIUrl}/trips/`, trip)
+    return this.http.post(`${this.APIUrl}/api/trips/`, trip)
   };
 
   // UPDATE TRIP
   updateTrip(value: any): Observable<object>{
-    return this.http.put(`${this.APIUrl}/trips/`, value);
+    return this.http.put(`${this.APIUrl}/api/trips/`, value);
   }
 
   // DELETE TRIP
   deleteTrip(id: number): Observable<any> {
-    return this.http.delete(`${this.APIUrl}/trips/${id}`)
+    return this.http.delete(`${this.APIUrl}/api/trips/${id}`)
   };
 
 }
