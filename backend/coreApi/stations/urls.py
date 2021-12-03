c92dd984016236425356dd7bf2f7666d62cd703f
Rochdi Khalid <87553163+rochdikhalid@users.noreply.github.com>
2021-12-03 11:44:06 +0100

    Update urls.py

--- backend/coreApi/stations/urls.py
+++ backend/coreApi/stations/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from stations import views
 
 urlpatterns = [
-    path('<int:id>/', views.stationApi),
+    path('api/stations/', views.stationApi),
+    path('api/stations/<int:id>/', views.stationApi),
 ]
