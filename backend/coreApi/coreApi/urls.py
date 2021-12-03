2e1eeba2bd8b69456e5f92ad038a9825ab2ba8f2
Rochdi Khalid <87553163+rochdikhalid@users.noreply.github.com>
2021-12-03 11:41:01 +0100

    Update urls.py

--- backend/coreApi/coreApi/urls.py
+++ backend/coreApi/coreApi/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/stations/', include('stations.urls')),
-    path('api/drivers/', include('drivers.urls')),
-    path('api/fleets/', include('fleets.urls')),
+    path('', include('stations.urls')),
+    path('', include('drivers.urls')),
+    path('', include('fleets.urls')),
+    path('', include('trips.urls')),
+    path('', include('passengers.urls')),
 ]
