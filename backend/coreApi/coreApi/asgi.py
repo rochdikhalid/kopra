8423883da81c4c0a06c1100b552692638cd97d90
Rochdi Khalid <87553163+rochdikhalid@users.noreply.github.com>
2021-12-14 10:11:35 +0100

    Update asgi.py

--- backend/coreApi/coreApi/asgi.py
+++ backend/coreApi/coreApi/asgi.py
@@ -1,16 +1,7 @@
-"""
-ASGI config for coreApi project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coreApi.settings')
 
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coreApi.settings')
 application = get_asgi_application()
