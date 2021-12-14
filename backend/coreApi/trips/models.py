152831b6c3f08f132623cc900ae9c8d0ee047425
Rochdi Khalid <87553163+rochdikhalid@users.noreply.github.com>
2021-12-14 10:28:04 +0100

    Update models.py

--- backend/coreApi/trips/models.py
+++ backend/coreApi/trips/models.py
@@ -2,19 +2,14 @@ from django.db import models
 
 
 
-
-
 # Stations Model
 class Trips(models.Model):
 
     # Trip from
     trip_from = models.CharField(max_length = 30)
-
     # Trip to
     trip_to = models.CharField(max_length = 30)
-
     # Duration
     duration = models.FloatField()
-
     # Distance
     distance = models.FloatField()
