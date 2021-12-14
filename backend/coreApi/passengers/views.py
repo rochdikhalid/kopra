132c2e5ffcbbbb4d0b5345ff7bea03fbe1dadf4f
Rochdi Khalid <87553163+rochdikhalid@users.noreply.github.com>
2021-12-14 10:21:02 +0100

    Update views.py

--- backend/coreApi/passengers/views.py
+++ backend/coreApi/passengers/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser
 from passengers.models import Passengers
 from passengers.serializers import PassengerSerializer
 
