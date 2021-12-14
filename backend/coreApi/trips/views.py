b3c4067d9908f9a499d876de59c697c38501adca
Rochdi Khalid <87553163+rochdikhalid@users.noreply.github.com>
2021-12-14 10:27:10 +0100

    Update views.py

--- backend/coreApi/trips/views.py
+++ backend/coreApi/trips/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser
 from trips.models import Trips
 from trips.serializers import TripSerializer
 
